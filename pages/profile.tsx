0220477a815df46bb02144458a4fc19b08c96bfd
Saurabh Kumar Suryan <saurabhsuryan99@hotmail.com>
2021-05-23 19:09:26 +0530

    ✨ adds a redirect

--- pages/profile.tsx
+++ pages/profile.tsx
@@ -1,6 +1,8 @@
+import { GetServerSideProps } from "next";
 import Head from "next/head";
 import Nav from "../components/Nav";
 import ProfilePage from "../components/Profile";
+import Cookie from "cookie";
 
 const Profile = () => {
   return (
@@ -14,6 +16,19 @@ const Profile = () => {
       </>
     </>
   );
+};
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const token = context.req.headers.cookie
+    ? Cookie.parse(context.req.headers.cookie).token
+    : null;
+
+  return {
+    redirect: {
+      destination: "/posts",
+      permanent: true,
+    },
+  };
 };
 
 export default Profile;
